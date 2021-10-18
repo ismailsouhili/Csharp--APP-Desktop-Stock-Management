14b30d377bbbb19c58bd9367ef84853e31c1593c
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 08:36:13 +0000

    Create UpdateCategory.cs

---
+++ stockmanagmentapp/CategoryForms/UpdateCategory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace stockmanagmentapp.mainforms
+{
+    public partial class UpdateCategory : Form
+    {
+        public UpdateCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection(@"Data Source= DESKTOP-OF1I649\SQLEXPRESS  ;initial catalog= StockManager  ;Integrated Security=True");
+            string req = string.Format("update Category set CategoryName='{0}'  where ID={1} ", txtcatname.Text,txtID.Text);
+            SqlCommand cmd = new SqlCommand(req, cnx);
+            cnx.Open();
+            int i = cmd.ExecuteNonQuery();
+            cnx.Close();
+
+            if (i == 1)
+            {
+
+                MessageBox.Show("Update successfully");
+                txtcatname.Text = string.Empty;
+                txtID.Text = string.Empty;
+
+            }
+            else
+                MessageBox.Show("Error");
+        }
+
+        private void bunifuButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
