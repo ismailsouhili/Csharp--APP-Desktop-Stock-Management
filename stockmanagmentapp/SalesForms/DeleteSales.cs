ccfa76b03d9ea309d444dc05c46ff850bfff5fc6
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 08:30:14 +0000

    Create DeleteSales.cs

---
+++ stockmanagmentapp/SalesForms/DeleteSales.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace stockmanagmentapp
+{
+    public partial class DeleteSales : Form
+    {
+        public DeleteSales()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bunifuButton2_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection(@"Data Source= DESKTOP-OF1I649\SQLEXPRESS  ;initial catalog= StockManager  ;Integrated Security=True");
+            string req = string.Format("delete from Sales where Refsales={0}", txtREF.Text);
+            SqlCommand cmd = new SqlCommand(req, cnx);
+            cnx.Open();
+            int i = cmd.ExecuteNonQuery();
+            cnx.Close();
+
+            if (i == 1)
+            {
+
+                MessageBox.Show("Delete successfully");
+                txtREF.Text = "";
+            }
+            else
+                MessageBox.Show("Error");
+        }
+    }
+}
