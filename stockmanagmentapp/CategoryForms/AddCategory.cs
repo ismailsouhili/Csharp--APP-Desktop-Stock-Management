badbff61fa0a18315be24df086468ebe1b3cae91
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2021-10-18 08:36:20 +0000

    Create AddCategory.cs

---
+++ stockmanagmentapp/CategoryForms/AddCategory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace stockmanagmentapp.mainforms
+{
+    public partial class AddCategory : Form
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void newcategoryform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection(@"Data Source= DESKTOP-OF1I649\SQLEXPRESS  ;initial catalog= StockManager  ;Integrated Security=True");
+            string req = string.Format("insert into Category values({0},'{1}')",txtID.Text,txtcatname.Text );
+            SqlCommand cmd = new SqlCommand(req, cnx);
+            cnx.Open();
+            int i = cmd.ExecuteNonQuery();
+            cnx.Close();
+
+            if (i == 1)
+            {
+
+                MessageBox.Show("Added successfully");
+                txtcatname.Text = string.Empty;
+               
+            }
+            else
+                MessageBox.Show("Error");
+        }
+    }
+}
